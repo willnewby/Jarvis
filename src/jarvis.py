8a45ee410cddc21f63cc4249dfa367e3b2c0ab27
AJ Minich <ajminich@gmail.com>
2011-11-19 02:14:53 -0800

    Adding logging capability to main Jarvis module.

--- src/jarvis.py
+++ src/jarvis.py
@@ -11,13 +11,15 @@ import logging
 # It's not threadsafe, but whatever
 status = True
 
+logger = logging.getLogger('Jarvis')
+
 # Bot message processor
 def messageHandler(conn, message):
     
     (reply, sendMessage, shutdown) = commands.getResponse(message)
     
     if shutdown:
-        print "Stop message received: shutting down bot."
+        logger.info("Stop message received: shutting down bot.")
         global status
         status = False
     elif sendMessage:
@@ -34,43 +36,48 @@ def serviceIterator(conn):
 
 if __name__ == '__main__':
 
-    logger = logging.getLogger('Jarvis')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s')
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
 
     if len(sys.argv)<3:
         print "Usage: jarvis.py username@server.net password"
         logger.error("Not enough arguments specified.")
-    else
-        jarvis()
+    else:
+    
+        jid = xmpp.protocol.JID(sys.argv[1])
+        user = jid.getNode()
+        server = jid.getDomain()
+        password = sys.argv[2]
+    
+        conn = xmpp.Client(server,debug=[])
+        conres = conn.connect(("talk.google.com", 5222))
         
-    jid = xmpp.protocol.JID(sys.argv[1])
-    user = jid.getNode()
-    server = jid.getDomain()
-    password = sys.argv[2]
-
-    conn = xmpp.Client(server,debug=[])
-    conres = conn.connect(("talk.google.com", 5222))
-    
-    if not conres:
-        print "Unable to connect to server %s!" % server
-        sys.exit(1)
-    if conres<>'tls':
-        print "Warning: unable to estabilish secure connection - TLS failed!"
-    authres=conn.auth(user,password)
-    if not authres:
-        print "Unable to authorize on %s - check login/password." % server
-        sys.exit(1)
-    if authres<>'sasl':
-        print "Warning: unable to perform SASL auth os %s. Old authentication method used!"%server
-    
-    # Initiate the message handler
-    conn.RegisterHandler('message',messageHandler)
-    
-    # Put bot online
-    conn.sendInitPresence()
-    
-    print "Good day sir."
-    
-    # Begin the service iterator to keep the bot going until the close message is received
-    serviceIterator(conn)
-    
-    print "Shutting down bot."
+        if not conres:
+            logger.error("Unable to connect to server %s!" % server)
+            sys.exit(1)
+        if conres<>'tls':
+            logger.warn("Unable to estabilish secure connection - TLS failed!")
+        
+        authres=conn.auth(user,password)
+        
+        if not authres:
+            logger.error("Unable to authorize on %s - check login/password." % server)
+            sys.exit(1)
+        if authres<>'sasl':
+            logger.warn("Warning: unable to perform SASL auth os %s. Old authentication method used!" % server)
+        
+        # Initiate the message handler
+        conn.RegisterHandler('message',messageHandler)
+        
+        # Put bot online
+        conn.sendInitPresence()
+        
+        logger.info("Good day sir. Ready for your command.")
+        
+        # Begin the service iterator to keep the bot going until the close message is received
+        serviceIterator(conn)
+        
+        logger.info("Have a good day, sir.")
